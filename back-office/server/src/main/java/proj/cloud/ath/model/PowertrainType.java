829d7f5c0f2ec0988b4b37aa122827a4332b64c0
w41k4z <alainricor@gmail.com>
2024-01-10 13:59:10 +0300

    Lombok integration

--- back-office/server/src/main/java/proj/cloud/ath/model/PowertrainType.java
+++ back-office/server/src/main/java/proj/cloud/ath/model/PowertrainType.java
@@ -5,7 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "powertrain_type")
 public class PowertrainType {
@@ -13,32 +19,6 @@ public class PowertrainType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
     private Integer state;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getState() {
-        return state;
-    }
-
-    public void setState(Integer state) {
-        this.state = state;
-    }
 }
