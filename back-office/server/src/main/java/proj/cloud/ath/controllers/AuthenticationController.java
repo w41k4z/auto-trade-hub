3aee264089f3ad1bd84c67a82e07218375ffcaea
w41k4z <alainricor@gmail.com>
2024-01-11 07:31:11 +0300

    Code refactoring and Spring security integration
    package model changed to entities, controller to controller, service to services...
    Controllers version controller added

---
+++ back-office/server/src/main/java/proj/cloud/ath/controllers/AuthenticationController.java
@@ -0,0 +1,12 @@
+package proj.cloud.ath.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = { "http://localhost:3000" })
+public class AuthenticationController {
+
+}
