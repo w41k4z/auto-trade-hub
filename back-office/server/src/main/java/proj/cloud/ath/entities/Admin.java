61ec2fb25d0babdea25bc67060df886633be353a
w41k4z <alainricor@gmail.com>
2024-01-12 22:15:38 +0300

    Jwt security set up

--- back-office/server/src/main/java/proj/cloud/ath/entities/Admin.java
+++ back-office/server/src/main/java/proj/cloud/ath/entities/Admin.java
@@ -1,13 +1,12 @@
 package proj.cloud.ath.entities;
 
 import java.sql.Date;
-import java.util.Collection;
-import java.util.List;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,49 +14,26 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Admin implements UserDetails {
+@Entity
+public class Admin {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "birth_date")
     private Date birthDate;
+
     private String email;
+
     private String password;
+
     private String phoneNumber;
+
     private Integer genre;
-
-    /*
-     * Lombok @Data annotation has already generated the getPassword() method
-     * so we don't need to implement it here for the UserDetails interface.
-     */
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ADMIN"));
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
---
+++ back-office/server/src/main/resources/script/test-data.sql
@@ -0,0 +1,2 @@
+insert into admin values
+(DEFAULT, 'Admin', NULL, '2000-01-01', 1, '+261 32 55 063 38', 'admin@gmail.com', '{bcrypt}$2a$10$N9qoBHQoIWmP.KhL8iD2/uXOYv3TjZ.KGgDYlM6rQ8yb1k9qG7a2');
