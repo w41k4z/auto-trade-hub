6c7455e5c9c9438a38cb8b641829b254fa4f47c7
w41k4z <alainricor@gmail.com>
2024-01-28 23:43:41 +0300

    Waiting for bug fixing

--- back-office/server/src/test/java/proj/cloud/ath/AutoTradeHubApplicationTests.java
+++ back-office/server/src/test/java/proj/cloud/ath/AutoTradeHubApplicationTests.java
@@ -3,9 +3,7 @@ package proj.cloud.ath;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import proj.cloud.ath.AutoTradeHubApplication;
-
-@SpringBootTest(classes = AutoTradeHubApplication.class)
+@SpringBootTest()
 class AutoTradeHubApplicationTests {
 
 	@Test
