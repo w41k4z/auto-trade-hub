a8963ae95735f41d1b40af3c5c1b53cb64f3442b
Mamison Rajaoharisaona <narymams@gmail.com>
2024-01-29 22:30:33 +0300

    last modification before merging

--- back-office/server/src/main/java/proj/cloud/ath/services/FavoriteService.java
+++ back-office/server/src/main/java/proj/cloud/ath/services/FavoriteService.java
@@ -3,7 +3,6 @@ package proj.cloud.ath.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import proj.cloud.ath.entities.Favorite;
