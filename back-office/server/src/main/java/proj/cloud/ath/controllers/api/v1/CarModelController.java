6c0f515227eac2b637dece05df2fe2b751f78418
DiarySoa <manjakarasolomanana@gmail.com>
2024-02-09 19:56:37 +0100

    removing client and server folder

--- back-office/server/src/main/java/proj/cloud/ath/controllers/api/v1/CarModelController.java
+++ back-office/server/src/main/java/proj/cloud/ath/controllers/api/v1/CarModelController.java
@@ -26,15 +26,9 @@ public class CarModelController {
     private JwtUtil jwtUtil;
 
     @GetMapping
-    public RestApiResponse findAll(@RequestHeader(value = "Authorization") String bearerToken) {
-        String token = bearerToken.substring(7);
+    public RestApiResponse findAll() {
         RestApiResponse response = new RestApiResponse();
-        if (jwtUtil.isValidToken(token)) {
             response = new RestApiResponse(service.findAll(), 200);
-        } else {
-            response.setMessage("Invalid token");
-            response.setStatus(403);
-        }
         return response;
     }
 
